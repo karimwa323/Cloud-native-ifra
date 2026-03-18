5340d94c1c2103a920a41e5669962fd7a3fe05ea
karimwa323 <karimwa323@gmail.com>
2026-03-18 04:16:16 +0200

    breast

--- infra-live/modules/IAM/output.tf
+++ infra-live/modules/IAM/output.tf
@@ -5,10 +5,10 @@ output "cluster_role_arn" {
 output "node_role_arn" {
   value = aws_iam_role.eks_nodes_role.arn
 }
-output "jenkins_profile_name" {
-  value = aws_iam_instance_profile.jenkins_profile.name
-}
+# output "jenkins_profile_name" {
+#   value = aws_iam_instance_profile.jenkins_profile.name
+# }
 
-output "jenkins_role_arn" {
-  value = aws_iam_role.jenkins_role.arn
-}
+# output "jenkins_role_arn" {
+#   value = aws_iam_role.jenkins_role.arn
+# }

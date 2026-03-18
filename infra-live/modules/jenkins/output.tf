a5879754d635aba75ba9a2f9129ed46b41db8a7c
karimwa323 <karimwa323@gmail.com>
2026-03-18 03:33:13 +0200

    delete jenkins VM

--- infra-live/modules/jenkins/output.tf
+++
@@ -1,4 +0,0 @@
-output "instance_id" {
-  description = "The ID of the Jenkins EC2 instance"
-  value       = aws_instance.jenkins.id
-}
--- infra-live/prod/jenkins/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "karim-eks-terraform-state-prod"
-    key            = "prod/jenkins/terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "terraform-state-lock-prod"
-    encrypt        = true
-  }
-}

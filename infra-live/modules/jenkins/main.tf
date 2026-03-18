6cd762362b8a7e02dd563b7577bfbbf95e092202
karimwa323 <karimwa323@gmail.com>
2026-03-18 04:19:46 +0200

    Destroy jenkins_vm

--- infra-live/modules/jenkins/main.tf
+++ infra-live/modules/jenkins/main.tf
@@ -1,81 +1,81 @@
-resource "aws_security_group" "jenkins_sg" {
-  name        = "${var.project_name}-jenkins-sg"
-  description = "Jenkins security group"
-  vpc_id      = var.vpc_id
+# resource "aws_security_group" "jenkins_sg" {
+#   name        = "${var.project_name}-jenkins-sg"
+#   description = "Jenkins security group"
+#   vpc_id      = var.vpc_id
 
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 8080
+#     to_port     = 8080
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    from_port   = 50000
-    to_port     = 50000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 50000
+#     to_port     = 50000
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
 
-  owners = ["099720109477"]
+#   owners = ["099720109477"]
 
-  filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-}
+#   filter {
+#     name = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+#   }
+# }
 
-resource "aws_instance" "jenkins" {
+# resource "aws_instance" "jenkins" {
 
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
+#   ami           = data.aws_ami.ubuntu.id
+#   instance_type = var.instance_type
+#   subnet_id     = var.subnet_id
 
-  iam_instance_profile = var.iam_instance_profile
+#   iam_instance_profile = var.iam_instance_profile
 
-  vpc_security_group_ids = [
-    aws_security_group.jenkins_sg.id
-  ]
+#   vpc_security_group_ids = [
+#     aws_security_group.jenkins_sg.id
+#   ]
 
-  key_name = var.key_name
+#   key_name = var.key_name
 
-  user_data = file("${path.module}/install_jenkins.sh")
+#   user_data = file("${path.module}/install_jenkins.sh")
 
-  tags = {
-    Name = "jenkins-master"
-  }
-}
+#   tags = {
+#     Name = "jenkins-master"
+#   }
+# }
 
-resource "aws_ebs_volume" "jenkins_data" {
-  availability_zone = "eu-central-1a"
-  size              = 20  
-  type              = "gp3"
+# resource "aws_ebs_volume" "jenkins_data" {
+#   availability_zone = "eu-central-1a"
+#   size              = 20  
+#   type              = "gp3"
 
-  tags = {
-    Name = "jenkins-data-disk"
-  }
-}
+#   tags = {
+#     Name = "jenkins-data-disk"
+#   }
+# }
 
-resource "aws_volume_attachment" "jenkins_att" {
-  device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.jenkins_data.id
-  instance_id = aws_instance.jenkins.id
-}
+# resource "aws_volume_attachment" "jenkins_att" {
+#   device_name = "/dev/sdh"
+#   volume_id   = aws_ebs_volume.jenkins_data.id
+#   instance_id = aws_instance.jenkins.id
+# }
--- infra-live/modules/jenkins/variables.tf
+++ infra-live/modules/jenkins/variables.tf
@@ -1,25 +1,25 @@
-variable "project_name" {
-  type = string
-}
+# variable "project_name" {
+#   type = string
+# }
 
-variable "vpc_id" {
-  type = string
-}
+# variable "vpc_id" {
+#   type = string
+# }
 
-variable "instance_type" {
-  type = string
-  default = "t3.large"
-}
+# variable "instance_type" {
+#   type = string
+#   default = "t3.large"
+# }
 
-variable "subnet_id" {
-  type = string
-}
+# variable "subnet_id" {
+#   type = string
+# }
 
-variable "key_name" {
-  type = string
-}
+# variable "key_name" {
+#   type = string
+# }
 
-variable "iam_instance_profile" {
-  description = "The name of the IAM instance profile for Jenkins"
-  type        = string
-}
+# variable "iam_instance_profile" {
+#   description = "The name of the IAM instance profile for Jenkins"
+#   type        = string
+# }
